684471129454ce252a66d530889a12b0cdb14ace
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-06-13 18:11:25 -0700

    reading and printing

--- files/files.go
+++ files/files.go
@@ -2,7 +2,13 @@ package files
 
 import (
   "io/ioutil"
-  
+  "fmt"
+  "os"
+  "path/filepath"
+  "strings"
+  "path"
+  "runtime"
+
 )
 
 func ReadFile(filename string) string {
@@ -13,4 +19,29 @@ func ReadFile(filename string) string {
 	}
 
 	return string(data)
+}
+
+func WriteToFile(file string, template []byte) os.FileInfo {
+	fileName := fmt.Sprintf("%s.html", strings.TrimSuffix(file, filepath.Ext(file)))
+
+	// err := ioutil.WriteFile(fileName, template, 0666)
+	err := ioutil.WriteFile(fileName, template, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fileInfo, err := os.Stat(fileName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return fileInfo
+}
+
+func RootDir() string {
+	_, b, _, _ := runtime.Caller(0)
+	d := path.Join(path.Dir(b))
+	return filepath.Dir(d)
 }
--- makesite.go
+++ makesite.go
@@ -4,8 +4,76 @@ package main
 import (
 	"fmt"
 	"makesite/files"
+	"html/template"
+	"bytes"
+	"os"
+	"log"
+	"path/filepath"
+
 )
 
+
+
 func main() {
-	fmt.Println(files.ReadFile("first-post.txt"))
+	RenderTemplate()
+}
+type data struct {
+	Content template.HTML
+}
+
+func print(text string) {
+	fmt.Println(text)
+}
+
+var templatesDir string = files.RootDir() + "/generated_templates"
+var templatePath string = files.RootDir() + "template.tmpl"
+
+var file string = "first-post.txt"
+
+
+func RenderTemplate() {
+	post_data := files.ReadFile(file)
+	contentType := data{Content: template.HTML(post_data)}
+
+
+	tmpl, err := template.ParseFiles("template.tmpl")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var buf bytes.Buffer
+
+	if err := tmpl.ExecuteTemplate(&buf, "template.tmpl", contentType); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := os.Stat(templatesDir); os.IsNotExist(err) {
+		if err = os.Mkdir(templatesDir, 0777); err != nil {
+			log.Printf("Error occured when creating generated_templates directory. Error: %v", err)
+		}
+	}
+
+
+	fileInfo := files.WriteToFile(templatesDir+"/"+filepath.Base(file), buf.Bytes())
+
+
+	fmt.Println(fileInfo)
+	//
+	// 	var buf bytes.Buffer
+	//
+	// 	if err := tmpl.ExecuteTemplate(&buf, "template.tmpl", contentType); err != nil {
+	// 			log.Fatal(err)
+	// 	}
+	//
+	// 		tmpl = buf.Bytes()
+	//
+	// 		if _, err := os.Stat(templatesDir); os.IsNotExist(err) {
+	// 			if err = os.Mkdir(templatesDir, 0777); err != nil {
+	// 				log.Printf("Error occured when creating generated_templates directory. Error: %v", err)
+	// 			}
+	// 		}
+	//
+	// 		fmt.Println(temp)
+
 }

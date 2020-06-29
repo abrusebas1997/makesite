bb2e707c56e492830eb9d9ab0c06d43cee41d89c
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-06-29 09:01:52 -0700

    done with requirements

--- makesite.go
+++ makesite.go
@@ -1,79 +1,43 @@
+
 package main
 
-
 import (
+	"flag"
 	"fmt"
-	"makesite/files"
 	"html/template"
-	"bytes"
+	"io/ioutil"
 	"os"
-	"log"
-	"path/filepath"
-
+	"github.com/labstack/gommon/color"
 )
 
+type FileCont struct {
+	Title   string
+	Content string
+}
 
+func generator() {
+	fileContents, err := ioutil.ReadFile("third-post.txt")
+
+	fmt.Println(string(fileContents))
+	if err != nil {
+		panic(err)
+	}
+
+	content := FileCont{
+		Title:   "first-post-w",
+		Content: string(fileContents),
+	}
+	t := template.Must(template.ParseFiles("template.tmpl"))
+
+	file, err := os.Create("third-post.html")
+	t.Execute(os.Stdout, content)
+	t.Execute(file, content)
+	file_flag := flag.String("latest-post.html", "defaultValue", ".txt")
+	flag.Parse()
+	fmt.Println("file:", *file_flag)
+}
 
 func main() {
-	RenderTemplate()
-}
-type data struct {
-	Content template.HTML
-}
-
-func print(text string) {
-	fmt.Println(text)
-}
-
-var templatesDir string = files.RootDir() + "/generated_templates"
-var templatePath string = files.RootDir() + "template.tmpl"
-
-var file string = "first-post.txt"
-
-
-func RenderTemplate() {
-	post_data := files.ReadFile(file)
-	contentType := data{Content: template.HTML(post_data)}
-
-
-	tmpl, err := template.ParseFiles("template.tmpl")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var buf bytes.Buffer
-
-	if err := tmpl.ExecuteTemplate(&buf, "template.tmpl", contentType); err != nil {
-		log.Fatal(err)
-	}
-
-	if _, err := os.Stat(templatesDir); os.IsNotExist(err) {
-		if err = os.Mkdir(templatesDir, 0777); err != nil {
-			log.Printf("Error occured when creating generated_templates directory. Error: %v", err)
-		}
-	}
-
-
-	fileInfo := files.WriteToFile(templatesDir+"/"+filepath.Base(file), buf.Bytes())
-
-
-	fmt.Println(fileInfo)
-	//
-	// 	var buf bytes.Buffer
-	//
-	// 	if err := tmpl.ExecuteTemplate(&buf, "template.tmpl", contentType); err != nil {
-	// 			log.Fatal(err)
-	// 	}
-	//
-	// 		tmpl = buf.Bytes()
-	//
-	// 		if _, err := os.Stat(templatesDir); os.IsNotExist(err) {
-	// 			if err = os.Mkdir(templatesDir, 0777); err != nil {
-	// 				log.Printf("Error occured when creating generated_templates directory. Error: %v", err)
-	// 			}
-	// 		}
-	//
-	// 		fmt.Println(temp)
-
+	color.Println(color.Green("Welcome to makesite!"))
+	generator()
 }

278cc2c7775412a34cc915e1a448fbf7f4e185f8
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-07-02 19:48:49 -0700

    from scratch

--- makesite.go
+++ makesite.go
@@ -1,43 +1,20 @@
-
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"strings"
 	"os"
 	"github.com/labstack/gommon/color"
 )
 
-type FileCont struct {
-	Title   string
+type create struct {
 	Content string
 }
 
-func generator() {
-	fileContents, err := ioutil.ReadFile("third-post.txt")
+func readFile(filename string) string {
 
-	fmt.Println(string(fileContents))
-	if err != nil {
-		panic(err)
-	}
-
-	content := FileCont{
-		Title:   "first-post-w",
-		Content: string(fileContents),
-	}
-	t := template.Must(template.ParseFiles("template.tmpl"))
-
-	file, err := os.Create("third-post.html")
-	t.Execute(os.Stdout, content)
-	t.Execute(file, content)
-	file_flag := flag.String("latest-post.html", "defaultValue", ".txt")
-	flag.Parse()
-	fmt.Println("file:", *file_flag)
-}
-
-func main() {
-	color.Println(color.Green("Welcome to makesite!"))
-	generator()
 }
